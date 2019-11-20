ad48128373e0ddaa5a1c30138632b46e2c66179e
[krkania] <[krkania@live.unc.edu]>
2019-11-20 11:11:57 -0500

    state it should be for presentation

--- app/src/androidTest/java/com/example/myapplication/DukeTest.java
+++ app/src/androidTest/java/com/example/myapplication/DukeTest.java
@@ -15,30 +15,92 @@ import static androidx.test.espresso.matcher.ViewMatchers.hasTextColor;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.*;
 
 public class DukeTest {
-//have to have
-    @Before
-    public void registerIdlingResource() {
-        // let espresso know to synchronize with background tasks
-        IdlingRegistry.getInstance().register(EspressoIdlingResource.getIdlingResource());
-    }
-    @After
-    public void unregisterIdlingResource() {
-        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.getIdlingResource());
-    }
+
+//    @Before
+//    public void registerIdlingResource() {
+//        // let espresso know to synchronize with background tasks
+//        IdlingRegistry.getInstance().register(EspressoIdlingResource.getIdlingResource());
+//    }
+//
+//    @After
+//    public void unregisterIdlingResource() {
+//        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.getIdlingResource());
+//    }
+
 
     @Rule
     public ActivityTestRule<Duke> activityTestRule = new ActivityTestRule<>(Duke.class);
 
-    @Test
-    public void UncButtonClick() {
-        onView(withId(R.id.uncBtn)).check(matches(isDisplayed())).perform(click());
-        onView(withId(R.id.uncPage)).check(matches(isDisplayed()));
-        onView(withId(R.id.UNCanswer)).check(matches(withText("YES!")));
-        onView(withId(R.id.UNCanswer)).check(matches(hasTextColor(R.color.black)));
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //    @Test
+//    public void stateButtonClick(){
+//        onView(withId(R.id.stateBtn)).check(matches(isDisplayed())).perform(click());
+//        //check that correct activity (page) is loaded
+//        onView(withId(R.id.statePage)).check(matches(isDisplayed()));
+//        //check textcontent
+//        onView(withId(R.id.stateAnswer)).check(matches(withText("...state who?")));
+//        //check textcolor
+//        onView(withId(R.id.stateAnswer)).check(matches(hasTextColor(R.color.white)));
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    @Test
+//    public void UncButtonClick() {
+//        onView(withId(R.id.uncBtn)).check(matches(isDisplayed())).perform(click());
+//        onView(withId(R.id.uncPage)).check(matches(isDisplayed()));
+//        onView(withId(R.id.UNCanswer)).check(matches(withText("YES!")));
+//        onView(withId(R.id.UNCanswer)).check(matches(hasTextColor(R.color.black)));
+//
+//    }
+
 
 }
--- app/src/androidTest/java/com/example/myapplication/MainActivityTest.java
+++ app/src/androidTest/java/com/example/myapplication/MainActivityTest.java
@@ -1,50 +1,66 @@
 package com.example.myapplication;
 
-
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-
-import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.rule.ActivityTestRule;
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.matcher.ViewMatchers.hasBackground;
 import static androidx.test.espresso.matcher.ViewMatchers.hasTextColor;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.ViewAction.*;
 import static androidx.test.espresso.action.ViewActions.click;
 
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
-import android.graphics.drawable.ColorDrawable;
-import android.view.View;
 
-import org.junit.Assert.*;
 public class MainActivityTest {
 
 
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    @Test
-    public void displaysButtonsOnLoad() {
-        onView(withId(R.id.dukeBtn)).check(matches(isDisplayed()));
-        onView(withId(R.id.uncBtn)).check(matches(isDisplayed()));
-        onView(withId(R.id.stateBtn)).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void checkTextOnStateClick(){
-        onView(withId(R.id.stateBtn)).check(matches(isDisplayed())).perform(click());
-        onView(withId(R.id.statePage)).check(matches(isDisplayed()));
-        onView(withId(R.id.stateAnswer)).check(matches(withText("...state who?")));
-        onView(withId(R.id.stateAnswer)).check(matches(hasTextColor(R.color.white)));
 
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    @Test
+//    public void displaysButtonsOnLoad(){
+//        onView(withId(R.id.uncBtn)).check(matches(isDisplayed()));
+//        onView(withId(R.id.stateBtn)).check(matches(isDisplayed()));
+//        onView(withId(R.id.dukeBtn)).check(matches(isDisplayed()));
+//    }
+//
+//
+//    @Test
+//    public void checkTextOnStateClick(){
+//        onView(withId(R.id.stateBtn)).check(matches(isDisplayed())).perform(click());
+//        onView(withId(R.id.statePage)).check(matches(isDisplayed()));
+//        onView(withId(R.id.stateAnswer)).check(matches(withText("...state who?")));
+//        onView(withId(R.id.stateAnswer)).check(matches(hasTextColor(R.color.white)));
+//    }
 
 
 
